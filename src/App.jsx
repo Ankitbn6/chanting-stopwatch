aefc7cce879dc4d96e0e034835e3019a32940343
Ankit Raj <ankitraj102004@gmail.com>
2023-11-01 03:21:02 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ function App() {
 
   return (
     <>
-      <h1 className='text-red-700 w-[250px] sm:w-[500px] md:w-[700px] lg:w-[850px] m-auto bg-white text-center text-4xl py-5'>Hare Krishna Hare Krishna Krishna Krishna Hare Hare <br/> Hare Rama Hare Rama Rama Rama Hare Hare</h1>
+      <img className='rounded-lg m-auto' src='https://theharekrishnamovement.files.wordpress.com/2020/10/mantra.jpg'/>
+      {/* <h1 className='text-red-700 w-[250px] sm:w-[500px] md:w-[700px] lg:w-[850px] m-auto bg-white text-center text-4xl py-5'>Hare Krishna Hare Krishna Krishna Krishna Hare Hare <br/> Hare Rama Hare Rama Rama Rama Hare Hare</h1> */}
       <Stopwatch />
     </>
   )
